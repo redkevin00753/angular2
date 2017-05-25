a73f26fc28b3349416ecf039a34f0b20c991abdb
liyang <dllyly@cn.ibm.com>
2017-05-25 13:37:57 +0800

    hero list

--- README.md
+++
@@ -1,2 +0,0 @@
-# angular2
-Learn angular2
--- angular-quickstart/app/loop-back.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'loop-back',
-  template: `
-    <input #box (keyup)="0">
-    <p>{{box.value}}</p>
-  `
-})
-export class LoopbackComponent { }
